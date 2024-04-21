79ff9419bc025f7df2a314b8f737bd935e1707d8
roshan200445 <ar21ecb0a02@student.nitw.ac.in>
2024-04-21 11:22:55 +0530

    query bot

---
+++ stream.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+def main():
+    st.set_page_config("Chat PDF")
+    st.header("Chat with Textbook")
+
+    user_question = st.text_input("Ask a Question from the PDF Files")
+
+    if user_question:
+        user_input(user_question)
+
+    with st.sidebar:
+        st.title("Menu:")
+        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
+        if st.button("Submit & Process"):
+            with st.spinner("Processing..."):
+                raw_text = get_pdf_text(pdf_docs)
+                text_chunks = get_text_chunks(raw_text)
+                get_vector_store(text_chunks)
+                st.success("Done")
+
+
+
+if __name__ == "__main__":
+    main()
