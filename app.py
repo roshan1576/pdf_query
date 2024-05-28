66bbb06890899feb39b5474352c09983702c1356
roshan_adarsandi0508 <139198219+roshan1576@users.noreply.github.com>
2024-05-28 22:25:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -98,23 +98,15 @@ def user_input(user_question):
 
 
 def main():
-    st.set_page_config("Chat PDF")
-    st.header("Chat with PDF")
+    st.set_page_config("Bhagavad-gita chatbot")
+    st.header("Chat with Bhagavad-gita")
 
-    user_question = st.text_input("Ask a Question from the PDF Files")
+    user_question = st.text_input("Ask a Question from Bhagavad-gita")
 
     if user_question:
         user_input(user_question)
 
-    with st.sidebar:
-        st.title("Menu:")
-        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
-        if st.button("Submit & Process"):
-            with st.spinner("Processing..."):
-                raw_text = get_pdf_text(pdf_docs)
-                text_chunks = get_text_chunks(raw_text)
-                get_vector_store(text_chunks)
-                st.success("Done")
+    
 
 
 
